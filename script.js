bd13e4c6a9aaeb46eb66c7311dd2a770423731c6
Isaque Roberto <isaquezulato@gmail.com>
2025-02-25 17:05:22 -0300

    correções

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-const searchInput = document.getElementById("search-input");
+const searchInput = document.getElementById('search-input');
 const resultArtist = document.getElementById("result-artist");
-const resultPlaylist = document.getElementById("result-playlists");
+const resultPlaylist = document.getElementById('result-playlists');
 
 function requestApi(searchTerm) {
-  const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((result) => displayResults(result));
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result, searchTerm));
 }
 
-function displayResults(result) {
-  resultPlaylist.classList.add("hidden");
-  const gridContainer = document.querySelector(".grid-container");
-  gridContainer.innerHTML = ""; // Limpa os resultados anteriores
+function displayResults(result, searchTerm) {
+    resultPlaylist.classList.add("hidden");
+    const gridContainer = document.querySelector('.grid-container');
+    gridContainer.innerHTML = ''; // Limpa os resultados anteriores    
 
-  const filteredArtists = result.filter((artist) => artist.name.toLowerCase().includes(searchTerm));
+    const filteredArtists = result.filter(artist => artist.name.toLowerCase().includes(searchTerm) );
 
-  filteredArtists.forEach((artist) => {
-    const artistCard = document.createElement("div");
-    artistCard.classList.add("artist-card");
 
-    artistCard.innerHTML = `
+  filteredArtists.forEach(artist => {    
+      const artistCard = document.createElement('div');
+      artistCard.classList.add('artist-card');
+
+      artistCard.innerHTML = `
           <div class="card-img">
               <img class="artist-img" src="${artist.urlImg}" />
               <div class="play">
@@ -32,10 +33,10 @@ function displayResults(result) {
               <span class="artist-categorie">Artista</span>
           </div>
       `;
-    gridContainer.appendChild(artistCard);
+       gridContainer.appendChild(artistCard);
   });
 
-  resultArtist.classList.remove("hidden");
+  resultArtist.classList.remove('hidden');
 }
 
 document.addEventListener('input', function () {
@@ -47,6 +48,5 @@ document.addEventListener('input', function () {
         return;
     }
 
-
-  requestApi(searchTerm);
+    requestApi(searchTerm);
 });
